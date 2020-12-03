2f9c8dba571cbb83aea7c4c61002ddcb19d87ceb
Klevis <66927289+klevi05@users.noreply.github.com>
2020-12-03 22:44:04 +0100

    updating code

--- gitAutomization.py
+++ gitAutomization.py
@@ -1,3 +1,5 @@
+import json
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -5,23 +7,27 @@ import time
 
 
 class GIT:
-    def __init__(self,kenga):
-        self.kenga = kenga
+    def __init__(self, repository_name, username, password):
+        self.repository_name = repository_name
+        self.username = username
+        self.password = password
 
-    def entry(self):
+    def Login(self):
         chrome_options = Options()
         chrome_options.headless = False
         driver = webdriver.Chrome(executable_path='C:\\webdriver\\chromedriver_win32\\chromedriver.exe', options=chrome_options)
-        driver.get("https://www.youtube.com/")
+        driver.get("https://www.github.com/")
         time.sleep(3)
-        search = driver.find_element_by_name('search_query')
-        button = driver.find_element_by_id('search-icon-legacy')
-        search.send_keys(self.kenga)
-        time.sleep(2)
-        button.send_keys(Keys.RETURN)
-        time.sleep(5)
-        driver.quit()
 
 
-start = GIT("the weekend")
-start.entry()
+with open('user.json', 'r') as P:
+    information = json.load(P)
+    context = information['te_dhena']
+    data = context[0]
+    username = data['username']
+    password = data['password']
+
+repository = input('Enter the repository name: ')
+start = GIT(repository,username,password)
+start.Login()
+
